762661d33fefa57dce28c10f459c7ae75810d3ab
hugoresende27 <Hugoresende27@gmail.com>
2021-10-13 23:19:45 +0100

    qua

---
+++ 001_onde_colocar_js/script.js
@@ -0,0 +1,12 @@
+function teste() 
+{
+    document.getElementById ('info').innerHTML ="O texto de Javascript";
+}
+
+/*
+vantagens de usar ficheiro separado:
+-separação do código
+-alteração de ficheiros js e html independentes
+-reutilização de código
+
+*/
