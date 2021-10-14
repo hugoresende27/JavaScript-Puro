2588d3810d664e135292ec97f55349f7327f33a3
hugoresende27 <Hugoresende27@gmail.com>
2021-10-14 12:07:28 +0100

    qui

--- 007_Funcoes/script.js
+++ 007_Funcoes/script.js
@@ -1,4 +1,20 @@
 function teste()
 {
-    
+    var x = 10;
+    var y = 20;
+    var res = x + y;
+    document.write(res);        //com document.write toda a estrutura HTML desparece e fica só o res 
+}
+
+function teste2()
+{
+    var final = adicionar(120,200);
+    document.getElementById('info2').innerHTML = final;
+}
+
+function adicionar( x , y )
+{   
+    var res = x + y;
+    //document.write (res);
+    return res;
 }
