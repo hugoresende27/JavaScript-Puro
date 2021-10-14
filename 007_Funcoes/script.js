4bfd97b474c56713110e79749503227f190a729a
hugoresende27 <Hugoresende27@gmail.com>
2021-10-14 22:12:54 +0100

    qui

--- 007_Funcoes/script.js
+++ 007_Funcoes/script.js
@@ -1,20 +1,44 @@
+//operadores aritmeticos --> sinais para executar operações matemáticas ou concatenar strings
+
+var x = 10, y = 20;
+x += y ; // x = 30
+
+//operadores de comparação --> para comparar variáveis ou valores, são usados em mecanismos de decisão de fluxo
+
+/*
+==      igual a valor
+===     igual em valor e em tipo
+!=      diferente de valor
+!==     diferente em valor ou em tipo
+?       operador ternário
+
+*/
+
+var nome = 'Hugo';
+
 function teste()
 {
     var x = 10;
-    var y = 20;
-    var res = x + y;
-    document.write(res);        //com document.write toda a estrutura HTML desparece e fica só o res 
+    var y = '10';
+    document.getElementById('vars').innerHTML= "VAR X:: "+x+" <br>VAR Y:: "+y;
+   
+    if ( x == y )
+    {
+        document.getElementById('info').innerHTML = "É igual ==";       //10 é == a '10'
+    }
+    else
+    {
+        document.getElementById('info').innerHTML = "As var são DIFERENTES";
+    }
+
+    if ( x === y )
+    {
+        document.getElementById('info2').innerHTML = "É igual ===";     //10 não é === a '10'
+    }
+    else
+    {
+        document.getElementById('info2').innerHTML = "As var são DIFERENTES ===";
+    }
 }
 
-function teste2()
-{
-    var final = adicionar(120,200);
-    document.getElementById('info2').innerHTML = final;
-}
-
-function adicionar( x , y )
-{   
-    var res = x + y;
-    //document.write (res);
-    return res;
-}
+  
