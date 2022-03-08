874d81650396dcf6490b702c2e9878d41e4b6ff4
hugoresende27 <Hugoresende27@gmail.com>
2022-03-08 12:38:17 +0000

    a

--- js_susana/exercicio2- alunos/exercicio2- alunos/js2/variáveis_globais.js
+++ js_susana/exercicio2- alunos/exercicio2- alunos/js2/variáveis_globais.js
@@ -1,19 +1,44 @@
+// var grupoCartas = [
+// // "🦄", 
+// // "🍦",
+// //  "🌈", 
+//  "👽", 
+//  "👾", 
+// //  "🤖", 
+// //  "👹", 
+// //  "👺",
+//  "🍇",
+//  "🕴️",
+//  "🙈",
+//  "🐖"];
+
+ 
+// var totalCartas=grupoCartas.concat(grupoCartas);
+
+var car1 = 'images/car1.jpg';
+var car2 = 'images/car2.jpg';
+var car3 = 'images/car3.jpg';
+var car4 = 'images/car4.jpg';
+var car5 = 'images/car5.jpg';
+var car6 = 'images/car6.jpg';
+var car7 = 'images/car7.jpg';
+var car8 = 'images/car8.jpg';
+
 var grupoCartas = [
-// "🦄", 
-// "🍦",
-//  "🌈", 
- "👽", 
- "👾", 
-//  "🤖", 
-//  "👹", 
-//  "👺",
- "🍇",
- "🕴️",
- "🙈",
- "🐖"];
+car1,
+car2,
+car3,
+car4,
+car5,
+car6,
+car7,
+car8,];
 
-
+ 
 var totalCartas=grupoCartas.concat(grupoCartas);
+
+
+
 var total = Number(0);
 
 document.getElementById('total').innerHTML = total;
@@ -45,7 +70,12 @@ function reset()
 
 function PlaySound(melody) {
     // alert("On Press of "+melody);
-    var path = "path\\to\\melody\\"
-    var snd = new Audio(path + melody + ".mp3");
+    var path = "video"
+    var snd = new Audio(path + melody + ".mp4");
     snd.play();
+}
+
+function duplica(){
+    // var totalCartas=grupoCartas.concat(grupoCartas,grupoCartas);
+    window.location.reload()
 }
